ae0091151f20103750e385cd1808fa5fcdb86904
Sun Xueli <145748644+shirley430316@users.noreply.github.com>
2024-04-24 23:15:15 +0800

    Update record.h

--- record.h
+++ record.h
@@ -1,6 +1,15 @@
+#include <iostream>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include <vector>
+#include <cmath>
+
 #ifndef STATUS_H
 #define STATUS_H
 
-void status(card pyramid[]);
+
 
 #endif
