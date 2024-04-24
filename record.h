4055f2a0a677ab40e21fded088713f1a55b23fd2
Sun Xueli <145748644+shirley430316@users.noreply.github.com>
2024-04-24 23:20:15 +0800

    Update record.h

--- record.h
+++ record.h
@@ -10,6 +10,10 @@
 #ifndef STATUS_H
 #define STATUS_H
 
-
+struct GameState;
+string recordGameState(GameState state);
+void revertToPreviousState(int currentStateIndex, vector<GameState> &record);
+void saveGameProgress(const string& fileName, vector<GameState> record);
+void loadGameProgress(int currentStateIndex, string& fileName);
 
 #endif
