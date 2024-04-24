fa0d776ebb9f920f2feb76e2c94bf7f16c4226af
uanxaw <158298089+uanxaw@users.noreply.github.com>
2024-04-24 20:19:19 +0800

    Update structures.h
    Changed "exist" parameter to "status" parameter. Added description for "status" parameter.

--- structures.h
+++ structures.h
@@ -1,10 +1,16 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 
+/*
+status:
+0: removed
+1: unremoved, unremovable
+2: unremoved, removable
+*/
 struct card {
 	char suit;
 	int num;
-	bool exist;
+	int status;
 };
 
 #endif
