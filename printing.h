051cd5c675f3713dda52818ecc66c908888a54a1
liwenbo0912 <145734945+Liwenbo2004@users.noreply.github.com>
2024-04-25 19:57:31 +0800

    Update printing.h

--- printing.h
+++ printing.h
@@ -8,7 +8,6 @@ void printcard_up(int status);
 void printcard_mid(int status, char suit, int num);
 void printcard_bottom(int status);
 void printmap(card map[]);
-void shuffle(card deck[], int len);
 void Initializing(card deck[]);
 void searchpyramid(card pyramid[], char s, int n);
 void print_deck(int &pos1, card deck[], int &oppo1);
