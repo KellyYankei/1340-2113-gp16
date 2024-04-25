fd3b281ad1cb2e240af1458010f3ae81a0df1333
Tu Jingtian <163427261+Tujt@users.noreply.github.com>
2024-04-25 20:19:39 +0800

    Update printing.cpp

--- printing.cpp
+++ printing.cpp
@@ -102,7 +102,7 @@ int searchpyramid(card pyramid[], char s, int n){
 	return -1
 }
 
-bool print_deck(int &pos1, card deck1[], int &oppo1, card show[2])   //pos1 means the position of the left card in the array "deck" defined below (not deck1)
+int print_deck(int pos1, card deck1[], int oppo1, card show[2])   //pos1 means the position of the left card in the array "deck" defined below (not deck1)
 {//show means the present card displayed. The status will be 1 normally, and will be 0 if there is no card showed in the corresponding position.
     card deck[20];    //use this array to record the remaining cards in the deck below  
     int num=0;       //use this to count the number of remaining cards in the deck below
@@ -133,7 +133,7 @@ bool print_deck(int &pos1, card deck1[], int &oppo1, card show[2])   //pos1 mean
 	    show[1]=deck1[0];
 	    show[0].status=0;
 	    show[1].status=0;
-	    return 0;
+	    return 2;
     }
     
     if(pos1>=1 && pos1<num){
