4a52f5c80af5cd1e456ddcca3fcb0572d6a1f479
Sun Xueli <145748644+shirley430316@users.noreply.github.com>
2024-04-26 20:17:42 +0800

    Update structures.h

--- structures.h
+++ structures.h
@@ -13,8 +13,6 @@ struct card {
 	int status;
 };
 
-bool operator==(const card& card1, const card& card2) {
-	return (card1.suit == card2.suit && card1.num == card2.num && card1.status == card2.status);
-}
+bool operator==(const card& card1, const card& card2);
 
 #endif
