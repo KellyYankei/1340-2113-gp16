388717786d370f82065394f3f9db125e7308ecda
KellyYankei <152520300+KellyYankei@users.noreply.github.com>
2024-04-26 14:54:58 +0800

    Update start.h

--- start.h
+++ start.h
@@ -1,25 +1,14 @@
+
 #include <stdio.h>
 #include <ncurses.h>
 #include <string>
+using namespace std;
 
 #define WIDTH 50
 #define HEIGHT 8
 
-int startx = 0;
-int starty = 0;
-int choose =1;
-int confirm =1;
-int highlight = 1;
-char const *mode[] = { 
-			"New Game",
-			"Start from the old Games",
-			"Records",
-			"Tutorial",
-			"Exit",
-		  };
-int n_choices = sizeof(mode) / sizeof(char *);
-void choose_mode();
-void print_menu(WINDOW *win, int highlight);
-int ask_confirm(int highlight,int starty,int startx);
-void print_confirm(WINDOW * win,int confirm,int highlight);
-int main();
+extern "C" int choose_mode();
+extern "C" void print_menu(WINDOW *win, int highlight);
+extern "C" int ask_confirm(int highlight,int starty,int startx);
+extern "C" void print_confirm(WINDOW * win,int confirm_mode,int highlight);
+extern "C" int start();
