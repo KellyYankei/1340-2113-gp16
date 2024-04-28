d47e764489a362098f93dd13e86e20892b9217a1
Sun Xueli <145748644+shirley430316@users.noreply.github.com>
2024-04-28 21:54:49 +0800

    Add files via upload

---
+++ reverse.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include "saveload.h"
+#include "actions.h"
+#include "structures.h"
+using namespace std;
+
+bool deleteLastStep(vector<action> record, card pyramid[21], card deck[15]){
+    if (record.size() == 0){
+        cout << "No previous step." << endl;
+        return false;
+    }
+    else {
+        action step = record.back();
+        int index1 = step.index1;
+        int index2 = step.index2;
+        switch (step.type){
+            case 1:
+                pyramid[index1].status = 2;
+            break;
+            case 2:
+                pyramid[index1].status = 2;
+                pyramid[index2].status = 2;
+            break;
+            case 3:
+                deck[index1].status = 2;
+            break;
+            case 4:
+                pyramid[index1].status = 2;
+                deck[index2].status = 2;
+            break;
+            case 5:
+                deck[index1].status = 2;
+                deck[index2].status = 2;
+            break;
+        }
+        record.pop_back();
+        return true;
+    }
+}
---
+++ reverse.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include "saveload.h"
+#include "actions.h"
+#include "structures.h"
+
+#ifndef REVERSE_H
+#define REVERSE_H
+
+bool deleteLastStep(vector<action> record, card pyramid[21], card deck[15]);
+
+#endif
