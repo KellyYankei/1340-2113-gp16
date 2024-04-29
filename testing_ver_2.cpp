2d83f1472294a7d508d6c240a5755e4720ce5c5f
liwenbo0912 <145734945+Liwenbo2004@users.noreply.github.com>
2024-04-29 22:30:27 +0800

    Update testing_ver_2.cpp

--- testing_ver_2.cpp
+++ testing_ver_2.cpp
@@ -16,6 +16,7 @@
 //#include "board.h"
 #include "reverse.h"
 #include "printing.h"
+#include "judgement.h"
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
@@ -23,36 +24,6 @@
 #define CLSCR   "\033c"
 
 using namespace std;
-
-void set_status(card pyramid[]){
-        for (int i = 0; i < 21; i++){
-                if (pyramid[i].status != 0)
-                        pyramid[i].status = 1;
-        }
-        if( (pyramid[0].status==1) && (pyramid[1].status==0) && (pyramid[2].status==0) ) pyramid[0].status=2;
-        if( (pyramid[1].status==1) && (pyramid[3].status==0) && (pyramid[4].status==0) ) pyramid[1].status=2;
-        if( (pyramid[2].status==1) && (pyramid[4].status==0) && (pyramid[5].status==0) ) pyramid[2].status=2;
-        if( (pyramid[3].status==1) && (pyramid[6].status==0) && (pyramid[7].status==0) ) pyramid[3].status=2;
-        if( (pyramid[4].status==1) && (pyramid[7].status==0) && (pyramid[8].status==0) ) pyramid[4].status=2;
-        if( (pyramid[5].status==1) && (pyramid[8].status==0) && (pyramid[9].status==0) ) pyramid[5].status=2;
-        if( (pyramid[6].status==1) && (pyramid[10].status==0) && (pyramid[11].status==0) ) pyramid[6].status=2;
-        if( (pyramid[7].status==1) && (pyramid[11].status==0) && (pyramid[12].status==0) ) pyramid[7].status=2;
-        if( (pyramid[8].status==1) && (pyramid[12].status==0) && (pyramid[13].status==0) ) pyramid[8].status=2;
-        if( (pyramid[9].status==1) && (pyramid[13].status==0) && (pyramid[14].status==0) ) pyramid[9].status=2;
-        if( (pyramid[10].status==1) && (pyramid[15].status==0) && (pyramid[16].status==0) ) pyramid[10].status=2;
-        if( (pyramid[11].status==1) && (pyramid[16].status==0) && (pyramid[17].status==0) ) pyramid[11].status=2;
-        if( (pyramid[12].status==1) && (pyramid[17].status==0) && (pyramid[18].status==0) ) pyramid[12].status=2;
-        if( (pyramid[13].status==1) && (pyramid[18].status==0) && (pyramid[19].status==0) ) pyramid[13].status=2;
-        if( (pyramid[14].status==1) && (pyramid[19].status==0) && (pyramid[20].status==0) ) pyramid[14].status=2;
-        if (pyramid[15].status==1) pyramid[15].status=2;
-        if (pyramid[16].status==1) pyramid[16].status=2;
-        if (pyramid[17].status==1) pyramid[17].status=2;
-        if (pyramid[18].status==1) pyramid[18].status=2;
-        if (pyramid[19].status==1) pyramid[19].status=2;
-        if (pyramid[20].status==1) pyramid[20].status=2;
-
-        return;
-}
 
 int main() {
     cout << CLSCR;
