9696eb84f172661781bce4c08566097852582754
liwenbo0912 <145734945+Liwenbo2004@users.noreply.github.com>
2024-04-29 19:57:22 +0800

    Update saveload.h

--- saveload.h
+++ saveload.h
@@ -6,7 +6,7 @@ using namespace std;
 #ifndef SAVELOAD_H
 #define SAVELOAD_H
 
-void save(int slot, card pyramid[21], card deck[15], vector<string> steps);
-void load(int slot, card pyramid[21], card deck[15], vector<string>& steps);
+void save(int slot, card pyramid[21], card deck[15], vector<action> steps);
+void load(int slot, card pyramid[21], card deck[15], vector<action>& steps);
 
 #endif
