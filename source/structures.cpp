859b98fcb4300e2e37f1f803d84437c782522491
Tu Jingtian <163427261+Tujt@users.noreply.github.com>
2024-04-30 18:33:22 +0800

    Update structures.cpp

--- source/structures.cpp
+++ source/structures.cpp
@@ -1,5 +1,6 @@
 #include "structures.h"
 
-bool operator==(const card& card1, const card& card2) {
+//This is used to define the "==" operator for judging if 2 card variable are totally the same for convenience.
+bool operator==(const card& card1, const card& card2) {  
 	return (card1.suit == card2.suit && card1.num == card2.num && card1.status == card2.status);
 }
